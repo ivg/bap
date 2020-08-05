a0279a17299e1548e804a5a122ba8924cf125eff
ivg <ivg@ieee.org>
2020-08-05 18:54:36 -0400

    wip

--- plugins/relocatable/rel_symbolizer.ml
+++ plugins/relocatable/rel_symbolizer.ml
@@ -9,6 +9,8 @@ include Self ()
 
 let width = Ogre.(require Image.Scheme.bits >>| Int64.to_int_trunc)
 
+type ref = Addr of Bitvec.t | Name of string
+
 module Plt : sig
   type t
   val create : Ogre.doc -> t
@@ -46,22 +48,27 @@ end
 module References : sig
   type t
   val create : Ogre.doc -> t
-  val lookup : t -> Bitvec.t -> string option
-  val search : t -> Word.t -> string option
+  val lookup : t -> Bitvec.t -> ref option
+  val search : t -> Word.t -> ref option
 end = struct
   open Image.Scheme
   open Ogre.Syntax
-  type t = string list Map.M(Bitvec_order).t
+  type t = ref list Map.M(Bitvec_order).t
 
   let empty = Map.empty (module Bitvec_order)
 
-  let collect =
+  let collect init map src =
     width >>| Bitvec.modulus >>= fun m ->
-    Ogre.collect Ogre.Query.(select (from external_reference)) >>|
-    Seq.fold ~init:empty ~f:(fun exts (addr, name) ->
-        Map.add_multi exts Bitvec.(int64 addr mod m) name)
+    Ogre.collect Ogre.Query.(select (from src)) >>|
+    Seq.fold ~init ~f:(fun exts (addr, value) ->
+        Map.add_multi exts Bitvec.(int64 addr mod m) (map m value))
 
-  let create doc = match Ogre.eval collect doc with
+  let name _ x = Name x and addr m x = Addr Bitvec.(int64 x mod m)
+  let extract =
+    collect empty name external_reference >>= fun names ->
+    collect names addr relocation
+
+  let create doc = match Ogre.eval extract doc with
     | Ok exts -> exts
     | Error err ->
       warning "Failed to obtain external references: %a" Error.pp err;
@@ -73,33 +80,105 @@ end = struct
   let search exts addr = lookup exts (Word.to_bitvec addr)
 end
 
-
-let agent = Knowledge.Agent.register
-    ~package:"bap" "rel-symbolizer"
+let plt_agent = Knowledge.Agent.register
+    ~package:"bap" "plt-symbolizer"
     ~desc:"extracts symbols from external relocations"
+    ~reliability:Knowledge.Agent.doubtful
+
+open KB.Syntax
+
+let optimize = Bil.fixpoint @@ Fn.compose
+    Bil.propagate_consts
+    Bil.fold_consts
+
+let collect_insns number_of_instructions entry =
+  let return bils = KB.return @@
+    optimize @@ List.(concat @@ rev bils) in
+  let rec collect bils addr collected =
+    if collected < number_of_instructions then
+      Theory.Label.for_addr addr >>= fun label ->
+      KB.collect Theory.Program.Semantics.slot label >>= fun insn ->
+      KB.collect Memory.slot label >>= function
+      | None -> return bils
+      | Some mem ->
+        let next = Addr.to_bitvec @@ Addr.succ @@ Memory.max_addr mem in
+        collect (Insn.bil insn :: bils) next (collected+1)
+    else return bils  in
+  collect [] entry 0
+
+let plt_size_of_arch : arch -> int option = function
+  | #Arch.arm -> Some 4
+  | #Arch.x86 -> Some 1
+  | #Arch.ppc -> Some 4
+  | _ -> None
+
+let plt_size label =
+  KB.collect Arch.slot label >>| plt_size_of_arch
+
+
+let find_reference = List.find_map ~f:(function
+    | Bil.Jmp (Load (_,Int dst,_,_))
+    | Bil.Jmp (Int dst)
+    | Bil.Move (_, Load (_,Int dst,_,_)) -> Some dst
+    | _ -> None)
 
 let resolve_plt_entries refs plt path =
-  let open KB.Syntax in
-  KB.propose agent Theory.Label.possible_name @@ fun label ->
+  KB.propose plt_agent Theory.Label.possible_name @@ fun label ->
   KB.collect Theory.Label.addr label >>=? fun addr ->
   KB.collect Theory.Label.unit label >>=? fun unit ->
   KB.collect Theory.Unit.path unit >>=? fun file ->
+  plt_size label >>=? fun size ->
   if file <> path || not (Plt.mem plt addr) then KB.return None
-  else
-    KB.collect Theory.Program.Semantics.slot label >>| fun insn ->
-    match Insn.bil insn with
-    | [Bil.Jmp (Load (_,Int dst,_,_))] ->
-      References.search refs dst
-    | _ -> None
+  else collect_insns size addr >>| fun bil ->
+    match find_reference bil with
+    | None ->
+      info "PLT at %a@\n%a@\n" Bitvec.pp addr Bil.pp bil;
+      None
+    | Some dst -> match References.search refs dst with
+      | Some (Name s) -> Some s
+      | _ -> None
+
+
+let label_for_ref = function
+  | Name s -> Theory.Label.for_name s
+  | Addr x -> Theory.Label.for_addr x
+
+let addresses mem =
+  let start = Memory.min_addr mem in
+  let len = Memory.length mem in
+  Seq.init len ~f:(Addr.nsucc start)
+
+let matches refs addr mem =
+  let addr =
+    Option.value_map addr ~default:Seq.empty ~f:Seq.singleton in
+  Seq.append addr (addresses mem) |>
+  Seq.find_map ~f:(References.search refs)
+
+let provide_dests refs plt path =
+  let (>>=?) x f = x >>= function
+    | None -> KB.return Insn.empty
+    | Some x -> f x in
+  KB.promise Theory.Program.Semantics.slot @@ fun label ->
+  KB.collect Theory.Label.addr label >>=? fun addr ->
+  KB.collect Theory.Label.unit label >>=? fun unit ->
+  KB.collect Memory.slot label >>=? fun mem ->
+  KB.collect Theory.Unit.path unit >>=? fun file ->
+  if file <> path || Plt.mem plt addr then KB.return Insn.empty
+  else collect_insns 1 addr >>| find_reference >>= fun addr ->
+    match matches refs addr mem with
+    | None -> KB.return Insn.empty
+    | Some ref ->
+      label_for_ref ref >>| fun dst ->
+      KB.Value.put Insn.Slot.dests Insn.empty @@
+      Some (Set.singleton (module Theory.Label) dst)
 
 let init () =
   Stream.observe (Stream.zip Project.Info.spec Project.Info.file)
   @@ fun (spec, file) ->
   let refs = References.create spec in
-  let syms = Symbolizer.create (References.search refs) in
   let plts = Plt.create spec in
   resolve_plt_entries refs plts file;
-  Symbolizer.provide agent (Symbolizer.set_path syms file)
+  provide_dests refs plts file
 
 let () =
   Config.manpage [
