28c8ef758b36517a11ca5bb389403f8d2526274e
oleg <forown@yandex.ru>
2020-08-05 13:45:46 -0400

    start to release

--- tools/deb-build.sh
+++ tools/deb-build.sh
@@ -5,44 +5,24 @@ set -e
 GITHUB=https://github.com/BinaryAnalysisPlatform/
 SOURCE=$GITHUB/bap
 BINDINGS=$GITHUB/bap-bindings
-BINARIES="bap bap-mc bapbundle"
+BINARIES="bap bapbundle bapbuild bap-mc"
 PREFIX=/usr/local
-VERSION=1.4.0
-OCAML=4.05.0
-LLVM_VERSION=5.0
 ARCH=$(dpkg-architecture -qDEB_BUILD_ARCH)
 
 CONFDIR=$PREFIX/etc/bap
 SWITCH=$(date +%s)
-OLDSWITCH="undefined"
-SIGURL=$GITHUB/bap/releases/download/v$VERSION/sigs.zip
 
 TMPDIR=$(mktemp -d)
 
-echo "Getting fresh OCaml compiler"
-
-if [ -d $HOME/.opam ]; then
-    OLDSWITCH=$(opam config var switch)
-    opam switch -A$OCAML $SWITCH
-else
-    opam init --comp=$OCAML
-    SWITCH=$OCAML
-fi
-
-
 eval $(opam config env)
 echo OCaml is at `which ocaml`
-echo LLVM is $LLVM_VERSION
-echo BAP version is $VERSION
 which ocaml
 
-echo "Pinning jbuilder to the not yet broken version"
-opam pin add jbuilder 1.0+beta17 -n --yes
-echo "Pinning utop to the latest not broken version"
-opam pin add utop 1.19.3 -n --yes
 echo "Looking in the dev-repo for the current list of dependencies"
 opam pin add bap --dev-repo --yes -n
-echo "Installing OCaml dependenices from OPAM"
+echo "Installing System dependenices"
+opam depext bap --yes
+echo "Installing OCaml dependenices"
 opam install --yes --deps-only bap
 echo "Installed dependencies. Cleaning up..."
 opam pin remove bap
@@ -56,12 +36,22 @@ sudo cp $(which ocamlfind) $PREFIX/bin
 
 
 cd bap-repo
+LLVM_VERSION=`opam config var conf-bap-llvm:package-version`
+LLVM_CONFIG=`opam config var conf-bap-llvm:config`
+
+VERSION=`git tag | tail -n 1 | sed "s/v//"`
+SIGURL=$GITHUB/bap/releases/download/v$BAP_VERSION/sigs.zip
+echo BAP version is $BAP_VERSION
+echo LLVM is $LLVM_VERSION
+
+sed -i "s/-j 2/-j 4/" oasis/common
+
 git pull
 ./configure --enable-everything \
-            --disable-ida --disable-fsi-benchmark \
+            --disable-ida \
             --with-llvm-version=$LLVM_VERSION \
             --with-llvm-config=llvm-config-$LLVM_VERSION \
-            --libdir=$(opam config var lib) \
+            --libdir=$PREFIX/lib/bap \
             --plugindir=$PREFIX/lib/bap \
             --prefix=$PREFIX \
             --sysconfdir=$CONFDIR
@@ -74,40 +64,56 @@ echo "Packing a bap debian package"
 
 sudo rm -rf bap
 
-mkdir -p bap/bap_$VERSION/DEBIAN
+mkdir -p bap/bap_$BAP_VERSION/DEBIAN
 
-BINDIR=bap/bap_$VERSION/$PREFIX/bin
-LIBDIR=bap/bap_$VERSION/$PREFIX/lib/bap
-SIGDIR=bap/bap_$VERSION/$PREFIX/share/bap
-DEBIAN=bap/bap_$VERSION/DEBIAN
+SHARED=bap/bap_$BAP_VERSION/$PREFIX/share
+BINDIR=bap/bap_$BAP_VERSION/$PREFIX/bin
+LIBDIR=bap/bap_$BAP_VERSION/$PREFIX/lib/bap
+SIGDIR=bap/bap_$BAP_VERSION/$PREFIX/share/bap
+DEBIAN=bap/bap_$BAP_VERSION/DEBIAN
+PRIMUS=$SHARED/primus/site-lisp
+BAPAPI=$SHARED/bap-api
 
-mkdir -p $BINDIR $LIBDIR $SIGDIR $DEBIAN
+mkdir -p $BINDIR $LIBDIR $SIGDIR $DEBIAN $PRIMUS $BAPAPI
 
 for binary in $BINARIES; do
-    cp $PREFIX/bin/$binary bap/bap_$VERSION/$PREFIX/bin
+    cp $PREFIX/bin/$binary bap/bap_$BAP_VERSION/$PREFIX/bin
 done;
 
 cp $PREFIX/lib/bap/*.plugin $LIBDIR
 curl -L $SIGURL > $SIGDIR/sigs.zip
 
+
+LISPSRC="primus_lisp primus_taint primus_test constant_tracker primus_symbolic_executor"
+for src in $LISPSRC; do
+    cp bap-repo/plugins/$src/lisp/*.lisp $PRIMUS/
+done;
+
+cp -r bap-repo/plugins/api/api/c $BAPAPI
+cp bap-repo/plugins/primus_systems/systems/*.asd $SHARED/primus
+
+
 cat > $DEBIAN/control <<EOF
 Package: bap
 Architecture: $ARCH
 Maintainer: Ivan Gotovchits
-Depends: libgmp10, zlib1g, libstdc++6
+Depends: libgmp10, zlib1g, libstdc++6, libtinfo5
 Priority: optional
-Version: $VERSION
+Version: $BAP_VERSION
 Description: Binary Analysis Platform
 EOF
 
-sudo chown -R root:root bap/bap_$VERSION
-dpkg-deb --build bap/bap_$VERSION
+sudo chown -R root:root bap/bap_$BAP_VERSION
+dpkg-deb --build bap/bap_$BAP_VERSION
 
 echo "now building the bindings"
 
 opam install ctypes ctypes-foreign --yes
 
 [ -d bap-bindings ] || git clone $BINDINGS bap-bindings
+
+
+sudo apt-get install autoconf # MOVE it?
 
 cd bap-bindings
 git pull
@@ -118,22 +124,22 @@ cd ..
 
 
 echo "Now packing libbap.deb"
-LIBDIR=bap/libbap_$VERSION/$PREFIX/lib
-DEBIAN=bap/libbap_$VERSION/DEBIAN
+LIBDIR=bap/libbap_$BAP_VERSION/$PREFIX/lib
+DEBIAN=bap/libbap_$BAP_VERSION/DEBIAN
 
 mkdir -p $LIBDIR $DEBIAN
 
 
-cp bap-bindings/_build/bap/libbap.so $LIBDIR/libbap.so.$VERSION
+cp bap-bindings/_build/bap/libbap.so $LIBDIR/libbap.so.$BAP_VERSION
 
 
 cat > $DEBIAN/control <<EOF
 Package: libbap
 Architecture: $ARCH
 Maintainer: Ivan Gotovchits
-Depends: libgmp10, zlib1g, libstdc++6, libffi6
+Depends: libgmp10, zlib1g, libstdc++6, libffi6, libtinfo5
 Priority: optional
-Version: $VERSION
+Version: $BAP_VERSION
 Description: Binary Analysis Platform C Library
 EOF
 
@@ -149,12 +155,12 @@ EOF
 
 chmod a+x $DEBIAN/postinst
 chmod a+x $DEBIAN/postrm
-sudo chown -R root:root bap/libbap_$VERSION
-dpkg-deb --build bap/libbap_$VERSION
+sudo chown -R root:root bap/libbap_$BAP_VERSION
+dpkg-deb --build bap/libbap_$BAP_VERSION
 
 echo "Now packing libbap-dev"
-HDRDIR=bap/libbap-dev_$VERSION/$PREFIX/include
-DEBIAN=bap/libbap-dev_$VERSION/DEBIAN
+HDRDIR=bap/libbap-dev_$BAP_VERSION/$PREFIX/include
+DEBIAN=bap/libbap-dev_$BAP_VERSION/DEBIAN
 
 mkdir -p $DEBIAN $HDRDIR
 cp bap-bindings/_build/bap/generated/bap.h $HDRDIR
@@ -165,13 +171,13 @@ Architecture: $ARCH
 Maintainer: Ivan Gotovchits
 Depends: libbap
 Priority: optional
-Version: $VERSION
+Version: $BAP_VERSION
 Description: Binary Analysis Platform C Library
 EOF
 
 cat > $DEBIAN/postinst <<EOF
 #!/bin/sh
-ln -sf $PREFIX/lib/libbap.so.$VERSION $PREFIX/lib/libbap.so
+ln -sf $PREFIX/lib/libbap.so.$BAP_VERSION $PREFIX/lib/libbap.so
 sudo ldconfig
 EOF
 
@@ -182,17 +188,10 @@ EOF
 
 chmod a+x $DEBIAN/postinst
 chmod a+x $DEBIAN/postrm
-sudo chown -R root:root bap/libbap-dev_$VERSION
-dpkg-deb --build bap/libbap-dev_$VERSION
+sudo chown -R root:root bap/libbap-dev_$BAP_VERSION
+dpkg-deb --build bap/libbap-dev_$BAP_VERSION
 
 echo "time to clean up..."
 cd bap-repo
 sudo sh -c "PATH=$PATH make uninstall"
 cd ..
-
-if [ $SWITCH != $OCAML ]; then
-    opam switch $OLDSWITCH
-    opam switch remove $SWITCH --yes
-else
-    rm -rf ~/.opam
-fi
