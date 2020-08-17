aeec7390829100f9b02021905e13650e8d26bace
oleg <forown@yandex.ru>
2020-08-17 17:02:48 -0400

    test

--- .github/actions/get-prerelease/run.sh
+++ .github/actions/get-prerelease/run.sh
@@ -4,22 +4,21 @@ set -e
 
 #TODO: change  the repo name!!!
 echo "get the releases"
-RELEASES=`curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/gitoleg/bap/releases | sed 's/\\n//g'`
-echo $RELEASES
+RELEASES=`curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/gitoleg/bap/releases`
 
 
 # check if the first argument in the list (the most recent one) is
 # a prerelease
 echo "get the latest prerelease flag"
-FIRST=`echo $RELEASES | jq '.[0]'`
+FIRST=`echo $RELEASES | jq -r '.[0]'`
 echo first $FIRST
 
-IsPRERELEASE=`echo $RELEASES | jq '.[0].prerelease'`
+IsPRERELEASE=`echo $RELEASES | jq -r '.[0].prerelease'`
 
 echo "check is it's true"
 PRERELEASE=
 if [ "check$IsPRERELEASE" = "checktrue" ]; then
-    PRERELEASE=`echo $RELEASES | jq '.[0].id'`
+    PRERELEASE=`echo $RELEASES | jq -r '.[0].id'`
 fi
 
 echo "::set-output name=id::$PRERELEASE"
