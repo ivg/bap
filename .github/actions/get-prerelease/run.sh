d0c3b646e1a2f85e35bea484fba2beeb6e87b43b
oleg <forown@yandex.ru>
2020-08-17 16:16:27 -0400

    test

--- .github/actions/get-prerelease/run.sh
+++ .github/actions/get-prerelease/run.sh
@@ -9,7 +9,8 @@ RELEASES=`curl -H "Accept: application/vnd.github.v3+json" https://api.github.co
 # check if the first argument in the list (the most recent one) is
 # a prerelease
 echo "get the latest prerelease flag"
-echo $RELEASES
+FIRST=`echo $RELEASES | jq '.[0]`
+echo first $FIRST
 
 IsPRERELEASE=`echo $RELEASES | jq '.[0].prerelease'`
 
