35c4f37ce76be0a22f53cefbea1d9a26374b7259
oleg <forown@yandex.ru>
2020-08-18 10:21:37 -0400

    test

--- .github/actions/get-prerelease/run.sh
+++ .github/actions/get-prerelease/run.sh
@@ -3,15 +3,12 @@
 set -e
 
 #TODO: change  the repo name!!!
-echo "get the releases"
 RELEASES=`curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/gitoleg/bap/releases`
-
 
 # check if the first argument in the list (the most recent one) is
 # a prerelease
 IsPRERELEASE=`echo "$RELEASES" | jq -r '.[0].prerelease'`
 
-echo "check is it's true"
 PRERELEASE=
 if [ "check$IsPRERELEASE" = "checktrue" ]; then
     PRERELEASE=`echo $RELEASES | jq -r '.[0].id'`
