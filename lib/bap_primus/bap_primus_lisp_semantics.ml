027241edabf1b5ecd1864d60d34581bbf3d5d1aa
ivg <ivg@ieee.org>
2021-02-01 17:11:01 -0500

    implements the meta-computation part of the compiler

--- lib/bap_primus/bap_primus_lisp_semantics.ml
+++ lib/bap_primus/bap_primus_lisp_semantics.ml
@@ -12,12 +12,13 @@ module Check = Bap_primus_lisp_type.Check
 module Key = Bap_primus_lisp_program.Items
 
 module Meta = struct
-  type state = {
-    bound : Set.M(Theory.Var.Top).t;
-    stack : (unit Theory.Var.t * Bitvec.t) list;
-    arith : (module Bitvec.S);
-  }
-  module State = struct type t = state end
+  module State = struct
+    type t = {
+      bound : int Map.M(Theory.Var.Top).t;
+      stack : (unit Theory.Var.t * unit Theory.Value.t) list;
+      arith : (module Bitvec.S);
+    }
+  end
   include Monad.State.T1(State)(KB)
   include Monad.State.Make(State)(KB)
 end
@@ -43,11 +44,14 @@ let program = KB.Class.property Theory.Source.cls "primus-lisp-program" @@
 
 let fail s = Meta.lift (KB.fail s)
 
-let lookup prog item name = match Resolve.semantics prog item name () with
+let lookup prog item name =
+  match Resolve.semantics prog item name () with
   | None -> !!None
   | Some (Error problem) ->
     fail (Unresolved_definition problem)
   | Some (Ok (fn,_)) -> !!(Some fn)
+
+let check_arg _ _ = true
 
 module Primitive = struct
   open KB.Syntax
@@ -82,7 +86,6 @@ let forget = Theory.Value.forget
 let create eff res =
   KB.Value.put Theory.Semantics.value eff (forget res)
 
-let res = KB.Value.get Theory.Semantics.value
 
 let symbol =
   KB.Class.property Theory.Value.cls "lisp-symbol" @@
@@ -90,16 +93,99 @@ let symbol =
     ~equal:String.equal
     ~inspect:(fun x -> Sexp.Atom x)
 
-let static =
+let static_slot =
   KB.Class.property Theory.Value.cls "static-value" @@
   KB.Domain.optional "bitvec"
     ~equal:Bitvec.equal
     ~inspect:(fun x -> Sexp.Atom (Bitvec.to_string x))
 
+let update_value r f =
+  let v = KB.Value.get Theory.Semantics.value r in
+  KB.Value.put Theory.Semantics.value r (f v)
+
+let set_static r x = update_value r @@ fun v ->
+  KB.Value.put static_slot v (Some x)
+
+let symsort = Bap_primus_value.Index.key_width
+let res = KB.Value.get Theory.Semantics.value
+let bits = Theory.Bitv.define
+let static x =
+  KB.Value.get static_slot (res x)
+
 let (!) = KB.(!!)
 
+
+module Stack = struct
+  open Meta.State
+
+  let is_bound v =
+    Meta.get () >>| fun {bound} ->
+    Map.mem bound (Theory.Var.forget v)
+
+  let lookup v =
+    let v = Theory.Var.forget v in
+    Meta.get () >>| fun {bound; stack} ->
+    if Map.mem bound (Theory.Var.forget v)
+    then List.Assoc.find stack v
+        ~equal:Theory.Var.Top.equal
+    else None
+
+
+  let push_var v x = function {bound; stack} as s -> {
+      s with
+      stack = (v,x) :: stack;
+      bound = Map.update bound v ~f:(function
+          | None -> 1
+          | Some n -> n+1)
+    }
+
+  let push v x =
+    let v = Theory.Var.forget v in
+    Meta.update @@ push_var v x
+
+  let rec update xs x z = match xs with
+    | [] -> []
+    | (x',_) :: xs when Theory.Var.Top.(x' = x) -> (x,z) :: xs
+    | xw :: xs -> xw :: update xs x z
+
+  let set v x =
+    let v = Theory.Var.forget v
+    and x = forget x in
+    Meta.update @@ function {stack} as s -> {
+        s with stack = update stack v x
+      }
+
+  let make_var word {data={exp=n; typ=t}} =
+    let s = match t with
+      | Any | Name _ -> word
+      | Symbol -> symsort
+      | Type m -> m in
+    Theory.Var.forget@@Theory.Var.define (bits s) n
+
+  let push_frame ws bs =
+    Meta.get () >>= fun s ->
+    let s,n = List.fold ~init:(s,0) bs ~f:(fun (s,n) (v,x) ->
+        push_var (make_var ws v) x s,n+1) in
+    Meta.put s >>| fun () ->
+    n
+
+  let rec pop_vars n s =
+    if n = 0 then s else match s.stack with
+      | [] -> failwith "bug: broken stack"
+      | (v,_) :: vs -> pop_vars (n-1) {
+          s with
+          stack = vs;
+          bound = Map.change s.bound v ~f:(function
+              | Some 1 | None -> None
+              | Some n -> Some (n-1))
+        }
+
+
+  let tear_frame size = Meta.update @@ fun s ->
+    pop_vars size s
+end
+
 module Prelude(CT : Theory.Core) = struct
-  let bits = Theory.Bitv.define
   let label = Meta.lift (KB.Object.create Theory.Program.cls)
 
   let rec seq = function
@@ -112,18 +198,18 @@ module Prelude(CT : Theory.Core) = struct
 
   let skip = CT.perform Theory.Effect.Sort.bot
   let pass = CT.perform Theory.Effect.Sort.bot
+  let empty = Theory.Effect.empty Theory.Effect.Sort.bot
 
   let pure res =
     res >>| fun res ->
-    create (Theory.Effect.empty Theory.Effect.Sort.bot) res
+    create empty res
 
   let bigint x m =
     let s = bits m in
     let m = Bitvec.modulus m in
     let x = Bitvec.(bigint x mod m) in
-    Meta.lift (CT.int s x)
-
-  let zero = bigint Z.zero 1
+    pure @@ Meta.lift (CT.int s x) >>| fun r ->
+    set_static r x
 
   let (:=) v x = Meta.lift@@CT.set v x
 
@@ -150,7 +236,8 @@ module Prelude(CT : Theory.Core) = struct
   let cast s x =
     Meta.lift@@CT.cast (bits s) CT.b0 !x
 
-  let undefined = full [] zero
+  let nil = !!(Theory.Value.empty Theory.Bool.t)
+  let undefined = full [] nil
 
   let unified x y f =
     Theory.Value.Match.(begin
@@ -165,18 +252,14 @@ module Prelude(CT : Theory.Core) = struct
       end)
 
 
-  let nil = pure @@ zero
-
-  let var n m =
-    Meta.lift@@CT.var@@Theory.Var.define (bits m) n
-
-  let symsort = Bap_primus_value.Index.key_width
-
-
-  let sym name =
-    Meta.return @@
-    KB.Value.put symbol (Theory.Value.empty (bits symsort)) (Some name)
-
+  let sym = function
+    | "nil" -> Meta.return@@set_static empty Bitvec.zero
+    | name ->
+      Meta.lift @@
+      KB.Symbol.intern name Theory.Program.cls >>|
+      KB.Object.id >>| Int63.to_int64 >>|
+      Bitvec.M64.int64 >>|
+      set_static empty
 
   let coerce_bits s x f =
     let open Theory.Value.Match in
@@ -196,14 +279,17 @@ module Prelude(CT : Theory.Core) = struct
       Meta.lift@@CT.non_zero !x >>= fun x -> f x in
     undefined
 
+
+
+
   let reify prog target name =
     let word = Theory.Target.bits target in
     let rec eval : ast -> unit Theory.Effect.t Meta.t = function
-      | {data=Int {data={exp=x; typ=Type m}}} -> pure@@bigint x m
-      | {data=Int {data={exp=x; typ=Any}}} -> pure@@bigint x word
-      | {data=Var {data={exp=n; typ=Type m}}} -> pure@@var n m
-      | {data=Var {data={exp=n; typ=Any}}} -> pure@@var n word
-      | {data=Sym {data=s}} -> pure@@sym s
+      | {data=Int {data={exp=x; typ=Type m}}} -> bigint x m
+      | {data=Int {data={exp=x; typ=Any}}} -> bigint x word
+      | {data=Var {data={exp=n; typ=Type m}}} -> var n m
+      | {data=Var {data={exp=n; typ=Any}}} -> var n word
+      | {data=Sym {data=s}} -> sym s
       | {data=Ite (cnd,yes,nay)} -> ite cnd yes nay
       | {data=Let ({data={exp=n; typ=Type t}},x,y)} -> let_ n t x y
       | {data=Let ({data={exp=n; typ=Any}},x,y)} -> let_ n word x y
@@ -213,45 +299,63 @@ module Prelude(CT : Theory.Core) = struct
       | {data=Set ({data={exp=n; typ=Any}},x)} -> set_ n word x
       | {data=Rep (cnd,body)} -> rep cnd body
       | _ -> undefined
+    and var n t =
+      let v = Theory.Var.define (bits t) n in
+      Stack.lookup v >>= function
+      | None -> pure@@Meta.lift@@CT.var v
+      | Some v -> pure (!!v)
     and ite cnd yes nay =
       let* cnd = eval cnd in
-      let* yes = eval yes in
-      let* nay = eval nay in
-      coerce_bool (res cnd) @@ fun cres ->
-      Meta.lift@@Theory.Var.fresh Theory.Bool.t >>= fun c ->
-      full [
-        !!cnd;
-        data [c := !cres];
-        Meta.lift@@CT.branch (CT.var c) !yes !nay;
-      ] @@
-      Meta.lift@@CT.ite (CT.var c) !(res yes) !(res nay)
+      match static cnd with
+      | Some cnd ->
+        if Bitvec.(equal cnd zero)
+        then eval nay
+        else eval yes
+      | None ->
+        coerce_bool (res cnd) @@ fun cres ->
+        Meta.lift@@Theory.Var.fresh Theory.Bool.t >>= fun c ->
+        let* yes = eval yes in
+        let* nay = eval nay in
+        full [
+          !!cnd;
+          data [c := !cres];
+          Meta.lift@@CT.branch (CT.var c) !yes !nay;
+        ] @@
+        Meta.lift@@CT.ite (CT.var c) !(res yes) !(res nay)
     and rep cnd body =
-      let* cnd = eval cnd in
-      let* body = eval body in
-      let* head = label and* loop = label and* tail = label in
-      coerce_bool (res cnd) @@ fun cres ->
-      full [
-        blk head [ctrl [Meta.lift@@CT.goto tail]];
-        blk loop [!!body];
-        blk tail [!!cnd; ctrl [
-            Meta.lift@@CT.branch !cres (CT.goto head) skip
-          ]]
-      ] !!cres
+      let* r = eval cnd in
+      match static r with
+      | Some x ->
+        if Bitvec.(equal x zero)
+        then !!r
+        else
+          eval body >>= fun _ ->
+          rep cnd body
+      | None ->
+        let* body = eval body in
+        let* head = label and* loop = label and* tail = label in
+        coerce_bool (res r) @@ fun cres ->
+        full [
+          blk head [ctrl [Meta.lift@@CT.goto tail]];
+          blk loop [!!body];
+          blk tail [!!r; ctrl [
+              Meta.lift@@CT.branch !cres (CT.goto head) skip
+            ]]
+        ] !!cres
     and app name xs =
       map xs >>= fun (aeff,xs) ->
-      let xs = List.map ~f:forget xs in
-      Meta.lift@@Primitive.eval name xs >>= fun peff ->
-      if KB.Domain.is_empty Theory.Semantics.domain peff
-      then
-        let* dst = Meta.lift@@Theory.Label.for_name name in
-        let* eff = args name xs in
-        full [
-          !!aeff;
-          !!eff;
-          ctrl [Meta.lift@@CT.goto dst]
-        ] !!(res eff)
-      else
+      match Resolve.defun check_arg prog Key.func name xs with
+      | None ->
+        Meta.lift@@Primitive.eval name xs >>= fun peff ->
         full [!!aeff; !!peff] !!(res peff)
+      | Some (Ok (fn,bs)) ->
+        Stack.push_frame word bs >>= fun size ->
+        eval (Def.Func.body fn) >>= fun eff ->
+        Stack.tear_frame size >>= fun () ->
+        !!eff
+      | Some (Error _) ->
+        assert false
+
     and map args =
       seq [] >>= fun eff ->
       Meta.List.fold args ~init:(eff,[]) ~f:(fun (eff,args) arg ->
@@ -260,29 +364,43 @@ module Prelude(CT : Theory.Core) = struct
           (eff,forget (res eff')::args)) >>| fun (eff,args) ->
       eff, List.rev args
     and seq_ xs =
-      nil >>= fun init ->
+      pure nil >>= fun init ->
       Meta.List.fold ~init xs ~f:(fun eff x  ->
           let* eff' = eval x in
           full [!!eff; !!eff'] !!(res eff'))
     and set_ n t x =
       let* eff = eval x in
       let v = Theory.Var.define (bits t) n in
-      coerce_bits (bits t) (res eff) @@ fun reff ->
-      full [!!eff; data [v := !reff]] !!reff
+      Stack.is_bound v >>= function
+      | true ->
+        Format.eprintf "the variable %s is bound@\n" n;
+        Stack.set v (res eff) >>= fun () ->
+        undefined
+      | false -> match static eff with
+        | Some _ ->
+          Format.eprintf "the rhs of %s is static@\n" n;
+          Stack.push v (res eff) >>= fun () ->
+          undefined
+        | None ->
+          Format.eprintf "A regular machine assignment of %s@\n" n;
+          coerce_bits (bits t) (res eff) @@ fun reff ->
+          full [!!eff; data [v := !reff]] !!reff
     and let_ v t x b =
       let* x = eval x in
       let* b = eval b in
       let v = Theory.Var.define (bits t) v in
-      coerce_bits (bits t) (res x) @@ fun rx ->
-      cast t rx >>= fun rx ->
-      full [
-        !!x;
-        data [v := !rx];
-        !!b;
-      ] !!(res b)
-    and args name xs =
-      sym name >>= fun x ->
-      Meta.lift@@Primitive.eval "invoke-subroutine" (forget x::xs) in
+      match static x with
+      | Some _ ->
+        Stack.push v (res x) >>= fun () ->
+        undefined
+      | None ->
+        coerce_bits (bits t) (res x) @@ fun rx ->
+        cast t rx >>= fun rx ->
+        full [
+          !!x;
+          data [v := !rx];
+          !!b;
+        ] !!(res b) in
     lookup prog Key.func name >>= function
     | Some fn -> eval (Def.Func.body fn)
     | None -> !!Insn.empty
@@ -343,9 +461,9 @@ let provide () =
         let module Arith = Bitvec.Make(struct
             let modulus = Bitvec.modulus bits
           end) in
-        let meta = Meta.{
+        let meta = Meta.State.{
             stack = [];
-            bound = Set.empty (module Theory.Var.Top);
+            bound = Map.empty (module Theory.Var.Top);
             arith = (module Arith);
           } in
         Theory.instance () >>= Theory.require >>= fun (module Core) ->
@@ -354,3 +472,5 @@ let provide () =
         res
 
 let enable () = provide ()
+
+let static = static_slot
