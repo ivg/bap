8ce77d38031de70b0ae2d975229903f01c0bb899
ivg <ivg@ieee.org>
2021-02-11 16:16:25 -0500

    adds the riscv target

---
+++ lib/bap_riscv/bap_riscv_target.ml
@@ -0,0 +1,52 @@
+open Core_kernel
+open Bap_core_theory
+
+let package = "bap"
+
+type r64 and r32 and r8
+
+type 'a bitv = 'a Theory.Bitv.t Theory.Value.sort
+
+let r64 : r64 bitv = Theory.Bitv.define 64
+let r32 : r32 bitv = Theory.Bitv.define 32
+let r8  : r8  bitv = Theory.Bitv.define 8
+
+let reg t n = Theory.Var.define t n
+let untyped = List.map ~f:Theory.Var.forget
+let (@<) xs ys = untyped xs @ untyped ys
+
+
+let regs = [
+  "zero";
+  "ra";
+  "sp";
+  "gp";
+  "tp";
+  "t0"; "t1"; "t2";
+  "s0"; "s1";
+  "a0"; "a1"; "a2"; "a3"; "a4"; "a5"; "a6"; "a7";
+  "s2"; "s3"; "s4"; "s5"; "s6"; "s7"; "s8"; "s9"; "s10"; "s11";
+  "t3"; "t4"; "t5"; "t6"
+]
+
+let array t = List.map regs ~f:(reg t)
+
+let parent = Theory.Target.declare ~package "riscv"
+
+let riscv t =
+  let mems = Theory.Mem.define t r8 in
+  let gpr = array t  in
+  let mem = reg mems "mem" in
+  let vars = gpr @< [mem] in
+  let bits = Theory.Bitv.size t in
+  let name = sprintf "riscv%d"  bits in
+  Theory.Target.declare ~package name ~parent
+    ~bits
+    ~code:mem
+    ~data:mem
+    ~vars
+
+let riscv64 = riscv r64
+let riscv32 = riscv r32
+
+let llvm_encoding = Theory.Language.declare ~package "llvm-riscv"
---
+++ plugins/riscv/riscv_main.ml
@@ -0,0 +1,66 @@
+open Bap_main
+open Bap.Std
+open Bap_core_theory
+open KB.Syntax
+module CT = Theory
+
+include Bap_main.Loggers()
+
+module Target = Bap_riscv_target
+module Dis = Disasm_expert.Basic
+
+
+
+
+(* the same target may have different encodings
+   or share encodings with other targets, in addition,
+   the encoding may differ per each instruction and even
+   be context-dependent, therefore target and encodings
+   are separate properties of different classes. In our
+   case, everything is trivial.
+*)
+let provide_decoding () =
+  KB.promise CT.Label.encoding @@ fun label ->
+  CT.Label.target label >>| fun t ->
+  if CT.Target.belongs Target.parent t
+  then Target.llvm_encoding
+  else CT.Language.unknown
+
+
+let enable_llvm () =
+  Dis.register Target.llvm_encoding @@ fun target ->
+  Dis.create ~backend:"llvm" @@
+  if Theory.Target.equal target Target.riscv64
+  then "riscv64"
+  else "riscv32"
+
+let enable_loader () =
+  let request_arch doc =
+    let open Ogre.Syntax in
+    match Ogre.eval (Ogre.request Image.Scheme.arch) doc with
+    | Error _ -> assert false
+    | Ok arch -> arch in
+  KB.promise CT.Unit.target @@ fun unit ->
+  KB.collect Image.Spec.slot unit >>| request_arch >>| function
+  | Some "riscv64" -> Target.riscv64
+  | Some "riscv32" -> Target.riscv32
+  | _ -> CT.Target.unknown
+
+
+let main _ctxt =
+  enable_llvm ();
+  enable_loader ();
+  provide_decoding ();
+  Ok ()
+
+(* semantic tags that describe what our plugin is providing,
+   setting them is important not only for introspection but
+   for the proper function of the cache subsystem.
+*)
+let provides = [
+  "riscv";
+]
+
+(* finally, let's register our extension and call the main function  *)
+let () = Bap_main.Extension.declare main
+    ~provides
