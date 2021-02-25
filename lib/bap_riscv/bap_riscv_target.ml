8fc138de5e00724992390a34865a4fdd883a7cfa
ivg <ivg@ieee.org>
2021-02-25 18:56:29 -0500

    implements riscv abi
    well at least some part of it, not yet integrated into the abi pass.

--- lib/bap_riscv/bap_riscv_target.ml
+++ lib/bap_riscv/bap_riscv_target.ml
@@ -30,22 +30,47 @@ let regs = [
 ]
 
 let array t = List.map regs ~f:(reg t)
-let x t = List.init 32 ~f:(fun i -> reg t (sprintf "X%d" i))
+let vars p t = List.init 32 ~f:(fun i -> reg t (sprintf "%c%d" p  i))
 
 let parent = Theory.Target.declare ~package "riscv"
     ~endianness:Theory.Endianness.le
 
+let select xs ~mask =
+  let mask = Set.of_list (module Int) mask in
+  List.filteri xs ~f:(fun i _ -> Set.mem mask i)
+
+let (--) x y = List.range x (y+1)
+
 let riscv t =
   let mems = Theory.Mem.define t r8 in
   let mem = reg mems "mem" in
-  let vars = x t @< [mem] in
+  let ints = untyped@@vars 'X' t in
+  let flts = untyped@@vars 'F' t in
+  let vars = ints @< flts @< [mem] in
   let bits = Theory.Bitv.size t in
-  let name = sprintf "riscv%d"  bits in
+  let name = sprintf "riscv%d" bits in
+  let x i = select ints ~mask:i in
+  let f i = select flts ~mask:i in
   Theory.Target.declare ~package name ~parent
     ~bits
     ~code:mem
     ~data:mem
     ~vars
+    ~regs:Theory.Role.Register.[
+        [general; integer], ints;
+        [general; floating], flts;
+        [constant; zero], x[0];
+        [function_argument], x(10--17) @ f(10--17);
+        [function_return], x(10--12) @ f(10--12);
+        [link], x[1];
+        [stack_pointer], x[2];
+        [thread], x[3];
+        [caller_saved], x[1] @ x(5--7) @ x(10--17) @ x(28--31) @
+                        f(0--7) @ f(10--17) @ f(28--31);
+        [callee_saved], x[2] @ x(8--9) @ x(18--27) @
+                        f(8--9) @ f(18--27);
+
+      ]
 
 let riscv64 = riscv r64
 let riscv32 = riscv r32
--- plugins/riscv/riscv_main.ml
+++ plugins/riscv/riscv_main.ml
@@ -1,3 +1,4 @@
+open Base
 open Bap_main
 open Bap.Std
 open Bap_core_theory
@@ -9,16 +10,13 @@ include Bap_main.Loggers()
 module Target = Bap_riscv_target
 module Dis = Disasm_expert.Basic
 
+let provides = [
+  "riscv";
+  "riscv64";
+  "riscv32";
+]
 
 
-
-(* the same target may have different encodings
-   or share encodings with other targets, in addition,
-   the encoding may differ per each instruction and even
-   be context-dependent, therefore target and encodings
-   are separate properties of different classes. In our
-   case, everything is trivial.
-*)
 let provide_decoding () =
   KB.promise CT.Label.encoding @@ fun label ->
   CT.Label.target label >>| fun t ->
@@ -45,24 +43,92 @@ let enable_loader () =
   | Some "riscv32" -> Target.riscv32
   | _ -> CT.Target.unknown
 
+module Abi = struct
+  open Bap_c.Std
+  open Bap.Std
+  open Monads.Std
+  open Monad.Option.Syntax
+  open Monad.Option.Let
+
+  let name = "riscv"            (* is there an official name? *)
+
+  let (.:()) file num = match Set.nth file num with
+    | None -> failwith "a wrong number of registers"
+    | Some v -> Var.reify v
+
+  let (.%()) file num = Bil.var file.:(num)
+
+  let is_floating = function
+    | `Basic {C.Type.Spec.t=#C.Type.real} -> true
+    | _ -> false
+
+  (* even x = x if x is even otherwise x+1 *)
+  let even x = x + x land 1
+
+  let data_model t =
+    let bits = Theory.Target.bits t in
+    new C.Size.base (if bits = 32 then `ILP32 else `LP64)
+
+  let insert_args t _sub _attrs {C.Type.Proto.return; args} =
+    let bits = Theory.Target.bits t in
+    let a = Theory.Target.regs t ~roles:Theory.Role.Register.[
+        integer; function_argument;
+      ] in
+    let fa = Theory.Target.regs t ~roles:Theory.Role.Register.[
+        floating; function_argument;
+      ] in
+    let regs = Set.length a in
+    let mem = Bil.var @@ Var.reify @@ Theory.Target.data t in
+    let* sp = Theory.Target.reg t Theory.Role.Register.stack_pointer >>| Var.reify in
+    let size = data_model t in
+    let stack t n =
+      size#bits t >>= Size.of_int_opt >>| fun sz ->
+      C.Abi.data size t,
+      Bil.load ~mem LittleEndian sz
+        ~addr:Bil.(var sp + int (Word.of_int ~width:bits n)) in
+    let param t n =
+      if n > regs then stack t (n - regs)
+      else
+        size#bits t >>= fun s ->
+        Monad.Option.guard (s <= 2 * bits) >>| fun () ->
+        C.Abi.data size t, match is_floating return,s <= bits with
+        | true,true -> fa.%(n)
+        | true,false -> Bil.concat fa.%(even n) fa.%(even n + 1)
+        | false,true -> a.%(n)
+        | false,false -> Bil.concat a.%(even n) a.%(even n + 1) in
+    let return = param return 0 in
+    let+ (_,params) = Monad.Option.List.fold args
+        ~init:(0,[]) ~f:(fun (used,pars) (_name,arg) ->
+            size#bits arg >>= fun argsz ->
+            param arg used >>| fun par ->
+            let used = if argsz <= bits then used + 1
+              else used + 2 + used land 1 in
+            used,par::pars) in
+    {C.Abi.return; params = List.rev params; hidden=[]}
+
+  let apply_headers proj =
+    let t = Project.target proj in
+    if Theory.Target.belongs Target.parent t then
+      let abi = C.Abi.{
+          insert_args = insert_args t;
+          apply_attrs = fun _ x -> x;
+        } in
+      C.Abi.register name abi;
+      let size = data_model t in
+      let apply_headers = C.Abi.create_api_processor size abi in
+      Bap_api.process apply_headers;
+      Project.set proj Bap_abi.name name
+    else proj
+end
+
 
 let main _ctxt =
   enable_llvm Target.llvm64 "riscv64";
   enable_llvm Target.llvm32 "riscv32";
   enable_loader ();
   provide_decoding ();
+  Bap_abi.register_pass Abi.apply_headers;
   Ok ()
 
-(* semantic tags that describe what our plugin is providing,
-   setting them is important not only for introspection but
-   for the proper function of the cache subsystem.
-*)
-let provides = [
-  "riscv";
-  "riscv64";
-  "riscv32";
-]
-
-(* finally, let's register our extension and call the main function  *)
 let () = Bap_main.Extension.declare main
     ~provides
